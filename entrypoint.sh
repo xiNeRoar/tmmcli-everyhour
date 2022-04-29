0978755222fa60dc338aac823fef58d45e8847da
xiNeRoar <66216186+xiNeRoar@users.noreply.github.com>
2022-04-29 02:58:33 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ ! -f /mnt/cron.conf ]; then
-#set time to 12:30AM run
-    echo "30 0 * * * /config/tinyMediaManagerCMD.sh -updateMovies -updateTv -scrapeUnscraped" > /mnt/cron.conf
+#scrap every hour
+    echo "0 0 0/1 1/1 * ? * /config/tinyMediaManagerCMD.sh -scrapeNew" > /mnt/cron.conf
 fi
 chmod 777 /config/*
 chmod 600 /mnt/cron.conf
