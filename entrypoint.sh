9af549dd3a4f31b0576e07a03ed389c835a383b3
xiNeRoar <66216186+xiNeRoar@users.noreply.github.com>
2023-07-28 20:01:10 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /mnt/cron.conf ]; then
 #scrap every hour
-    echo "0 * * * * /config/tinyMediaManagerCMD.sh -updateMovies -scrapeNew" > /mnt/cron.conf
+    echo "0 * * * * /config/tinyMediaManagerCMD.sh -scrapeUnscraped -updateMovies -updateTv -scrapeNew" > /mnt/cron.conf
 fi
 chmod 777 /config/*
 chmod 600 /mnt/cron.conf
